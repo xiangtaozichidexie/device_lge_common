7d84f627fd597e8e73755a3bfa45a4b1d13b0690
xiangtaozichidexie <160228282+xiangtaozichidexie@users.noreply.github.com>
2024-02-16 18:44:10 +0800

    Upload modified files from my computer(from anht3889)

---
+++ dac_panel/src/org/lineageos/settings/device/dac/QuadDACPanelActivity.java
@@ -0,0 +1,19 @@
+package org.lineageos.settings.device.dac;
+
+import android.os.Bundle;
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.widget.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QuadDACPanelActivity extends CollapsingToolbarBaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+                new QuadDACPanelFragment()).commit();
+    }
+
+}
